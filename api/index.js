9201eb90ad38856c7f5ba7e7a8eff12c8ef85657
Zakaria aourzag <z.aourzag@gmail.com>
2021-11-02 10:27:13 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -65,7 +65,11 @@ export async function decodeJWT(jwt) {
 
 export async function getImages(broadcaster_id, guest_id) {
   const ret = {}
-
+ var sql = "CREATE TABLE images (name VARCHAR(255), address VARCHAR(255))";
+  query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
   const { results } = await query(`SELECT inactive, speaking FROM images WHERE discord_id = ?`, [guest_id])
   if (results && results.length) {
     ret.inactive = results[0].inactive
